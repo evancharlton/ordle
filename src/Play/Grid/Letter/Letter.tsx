43a2796273ca385469ac5d0b763105542c76a19b
Evan Charlton <evancharlton@gmail.com>
2022-01-15 10:20:03 +0100

    Clean up the help dialog a bit

--- src/Play/Grid/Letter/Letter.tsx
+++ src/Play/Grid/Letter/Letter.tsx
@@ -3,12 +3,13 @@ import classes from "./Letter.module.css";
 type Props = {
   letter: string;
   mode: "yes" | "maybe" | "no" | "unknown";
-};
+} & Pick<React.HTMLAttributes<HTMLDivElement>, "className">;
 
-const Letter = ({ letter, mode }: Props) => {
+const Letter = ({ letter, mode, className }: Props) => {
   return (
     <div
       className={[
+        className,
         classes.letter,
         mode === "yes" && classes.yes,
         mode === "maybe" && classes.maybe,
