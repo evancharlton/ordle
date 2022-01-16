e2397dc7cc21af8cb043ed25f6cc49383e311910
Evan Charlton <evancharlton@microsoft.com>
2022-01-16 09:03:34 +0100

    Remove the "Norsk" prefix
    This can come back when/if I add other languages.

--- src/App/Setup/LanguageOptions/LanguageOptions.tsx
+++ src/App/Setup/LanguageOptions/LanguageOptions.tsx
@@ -4,8 +4,8 @@ import classes from "./LanguageOptions.module.css";
 const LanguageOptions = () => {
   return (
     <div className={classes.container}>
-      <Link to="/nb-no">Norsk (bokmål)</Link>
-      <Link to="/nn-no">Norsk (nynorsk)</Link>
+      <Link to="/nb-no">bokmål</Link>
+      <Link to="/nn-no">nynorsk</Link>
     </div>
   );
 };
