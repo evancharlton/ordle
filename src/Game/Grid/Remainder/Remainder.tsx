8cc7227296b3a85ac011d4041c1e5301d01e8f29
Evan Charlton <evancharlton@gmail.com>
2024-10-19 09:40:31 +0200

    chore: Clean up inconsistent exports
    There was a warning about Fast Refresh being broken because a file that
    exported a component didn't **only** export components.

--- src/Game/Grid/Remainder/Remainder.tsx
+++ src/Game/Grid/Remainder/Remainder.tsx
@@ -1,40 +1,6 @@
-import { useMemo } from "react";
-import { useWords } from "../../../App/Setup/DataLoader";
-import { useWord } from "../../../App/Setup/GameLoader";
-import { getLegality } from "../../../utils";
-import { useGuesses, useSettings } from "../../guess";
+import { useGuesses } from "../../guess";
 import classes from "./Remainder.module.css";
-
-const format = (v: number) => {
-  if (v < 1_000) {
-    return v;
-  }
-  const thousands = Math.floor(v / 1000);
-  const hundreds = `000${v % 1000}`.substr(-3);
-  return `${thousands}${String.fromCharCode(0xa0)}${hundreds}`;
-};
-
-export const usePossibilities = (guesses: string[]) => {
-  const { showRemaining } = useSettings();
-
-  const words = useWords();
-  const word = useWord();
-  return useMemo(() => {
-    if (!showRemaining) {
-      return {
-        remainders: [],
-        formattedCount: format(0),
-      };
-    }
-
-    let filtered = [...words];
-    guesses.forEach((guess) => {
-      const test = getLegality(word, guess);
-      filtered = filtered.filter((w) => test(w) === "legal");
-    });
-    return { remainders: filtered, formattedCount: format(filtered.length) };
-  }, [guesses, showRemaining, word, words]);
-};
+import { usePossibilities } from "./usePossibilities";
 
 const Remainder = () => {
   const [guessMap] = useGuesses();
--- src/Game/Grid/Remainder/index.ts
+++ src/Game/Grid/Remainder/index.ts
@@ -1,2 +1,2 @@
 export { default } from "./Remainder";
-export { usePossibilities } from "./Remainder";
+export { usePossibilities } from "./usePossibilities";
